a0025c2eac30e5dbd903341c8bdd9489f04a1179
Agustín Ramiro Díaz <agustin.ramiro.diaz@gmail.com>
2023-09-07 17:43:28 -0300

    set base path
    Signed-off-by: Agustín Ramiro Díaz <agustin.ramiro.diaz@gmail.com>

--- _config.ts
+++ _config.ts
@@ -3,6 +3,7 @@ import plugins from "./plugins.ts";
 
 const site = lume({
   src: "./src",
+  location: new URL("https://agustinramirodiaz.github.io/theme-simple-blog/"),
 });
 
 site.use(plugins());
